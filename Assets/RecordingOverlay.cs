056e4b34330cd8776693bb7fca65c95956cf5e2b
Oscar <oscar.shrimpton.personal@gmail.com>
2017-11-22 17:38:26 +0000

    Recording text now flashes

---
+++ Assets/RecordingOverlay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordingOverlay : MonoBehaviour {
+
+	public Text text;
+
+	public float timeBetweenFlashes;
+
+	float timeSinceFlash;
+	
+	string prevText;
+
+	// Update is called once per frame
+	void Update () {
+		timeSinceFlash += Time.deltaTime;
+
+		if (timeSinceFlash >= timeBetweenFlashes)
+		{
+			if (text.text == "")
+				text.text = prevText;
+			else{
+				prevText = text.text;
+				text.text = "";
+			}
+			timeSinceFlash = 0f;
+		}
+	}
+}
