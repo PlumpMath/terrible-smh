7ba73da67cd37f3634230eb78bbc7bf74a1319d7
Oscar <oscar.shrimpton.personal@gmail.com>
2017-11-22 19:07:11 +0000

    Main Menu

---
+++ Assets/MainMenuCameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCameraScript : MonoBehaviour {
+
+	public Vector3 startPoint;
+
+	public Vector3 endPoint;
+
+	public float speed;
+
+	void Update () {
+		Vector3 newPos = transform.position;
+
+		if (newPos == endPoint)
+		{
+			newPos = startPoint;
+		}else{
+			newPos.x += speed;
+			if (newPos.x >= endPoint.x)
+			{
+				newPos = endPoint;
+			}
+		}
+
+		transform.position = newPos;
+	}
+}
